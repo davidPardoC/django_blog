a8c2161d952b43be9f7b8db91cfe3457be19d725
David Pardo <david.pardo@rappi.com>
2023-08-18 17:49:46 -0500

    Add templates initial and bootstrap

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'title': 'First post',
+        'content': 'This is the first post.',
+        'author': 'John Doe',
+        'date_posted': '2020-01-01'
+    },
+    {
+        'title': 'Second post',
+        'content': 'This is the second post.',
+        'author': 'Jane Doe',
+        'date_posted': '2020-01-02'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {'posts': posts, 'title': 'Home'}
+    return render(request, 'blog/home.html', context=context)
 
 
 def about(request):
