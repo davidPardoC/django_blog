103301ff803ba8ebf893644a2ed23863020e1a1e
David Pardo <david.pardo@rappi.com>
2023-08-19 12:50:09 -0500

    Add blog post db query on views

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-posts = [
-    {
-        'title': 'First post',
-        'content': 'This is the first post.',
-        'author': 'John Doe',
-        'date_posted': '2020-01-01'
-    },
-    {
-        'title': 'Second post',
-        'content': 'This is the second post.',
-        'author': 'Jane Doe',
-        'date_posted': '2020-01-02'
-    }
-]
+from .models import Post
 
 
 def home(request):
+    posts = Post.objects.all()
     context = {'posts': posts, 'title': 'Home'}
     return render(request, 'blog/home.html', context=context)
 
