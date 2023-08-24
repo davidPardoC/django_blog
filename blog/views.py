ce31ab4866d7658bb5daace425b030ad2e527a13
David Pardo <pardodavid10@gmail.com>
2023-08-23 22:03:23 -0500

    Change to class based view

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 
 def home(request):
     posts = Post.objects.all()
     context = {'posts': posts, 'title': 'Home'}
     return render(request, 'blog/home.html', context=context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] 
 
 
 def about(request):
