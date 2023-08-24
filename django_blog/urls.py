f96cd4fb2b75dca274aa403959b5ee09aa22b776
David Pardo <pardodavid10@gmail.com>
2023-08-23 21:08:49 -0500

    Add profile view

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as users_views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +32,7 @@ urlpatterns = [
          auth_views.LogoutView.as_view(template_name="users/logout.html"),
          name='logout'),
     path('', include('blog.urls')),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
